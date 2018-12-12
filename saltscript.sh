f8d93c19776041f19a04b59e5bd07b6e4b9261cd
neziriemin <neziriemin1@gmail.com>
2018-12-12 12:27:06 +0000

    paketointi

--- saltscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright 2018 Tero Karvinen http://TeroKarvinen.com GPL 3
-# Edited by Emin Neziri
-echo "Updating packages and installing salt-minion"
-sudo apt -y update
-sudo apt -y install salt-minion
-echo -e 'master: 192.168.1.83 \nid: orja' |sudo tee /etc/salt/minion
-sudo systemctl restart salt-minion
